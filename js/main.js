360bfd8c88bc97541e865560757c1affb2d77066
Nicole Dicochea <dicocheanicole@gmail.com>
2022-10-16 19:06:30 -0700

    added ingredients

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ function getDrink() {
 
     let drink = document.querySelector('input').value
     drink = drink.toLowerCase()
+    drink = drink.split('').join('')
 
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`
+    const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + drink
 
     // query parameters
 
@@ -21,10 +22,23 @@ function getDrink() {
             document.querySelector('img').src = data.drinks[random].strDrinkThumb;
             document.querySelector('h3').innerText = data.drinks[random].strInstructions;
 
+            for (const keys in data.drinks[random]) {
+                if (keys.includes('strIngredient')) {
+                    if(data.drinks[random][keys] != null) {
+                        // console.log(data.drinks[random][keys])
+                        document.querySelector('ul').appendChild(document.createElement('li')).innerText = data.drinks[random][keys];
+                    }
+                }  
+            }
+            document.querySelector('ul').appendChild(document.createElement('li')).innerText = data.drinks[random].strIngredient1;
+            
+
         })
         .catch(err => {
             console.log(`error ${err}`)
         })
+
+        document.querySelector('ul').innerText = null
     
 }
 
